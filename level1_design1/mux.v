eabfdd2df0aff2a880f84e8281996b3dcfe8011b
RaviMugidi <ravimugidi7@gmail.com>
2022-07-22 17:18:13 +0000

    updated testfile

--- level1_design1/mux.v
+++ level1_design1/mux.v
@@ -12,8 +12,8 @@ module mux(sel,inp0, inp1, inp2, inp3, inp4, inp5, inp6, inp7, inp8,
             inp21, inp22, inp23, inp24, inp25, inp26,
             inp27, inp28, inp29, inp30;
 
-  output [1:0] out;
-  reg [1:0] out;
+  output reg [1:0] out;
+  //reg [1:0] out;
 
   // Based on sel signal value, one of the inp0-inp30 gets assigned to the 
   // output signal
@@ -37,7 +37,7 @@ module mux(sel,inp0, inp1, inp2, inp3, inp4, inp5, inp6, inp7, inp8,
       5'b01001: out = inp9;  
       5'b01010: out = inp10;
       5'b01011: out = inp11;
-      5'b01101: out = inp12;
+      5'b01100: out = inp12;//error
       5'b01101: out = inp13;
       5'b01110: out = inp14;
       5'b01111: out = inp15;
@@ -55,6 +55,7 @@ module mux(sel,inp0, inp1, inp2, inp3, inp4, inp5, inp6, inp7, inp8,
       5'b11011: out = inp27;
       5'b11100: out = inp28;
       5'b11101: out = inp29;
+      5'b11110: out = inp30;//added
       default: out = 0;
     endcase
   end
--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -38,7 +38,7 @@ async def test_mux(dut):
     in29=1
     in30=2
     
-    sel=15
+    Sel=12
 
     dut.inp0.value=in0
     dut.inp1.value=in1
@@ -72,69 +72,71 @@ async def test_mux(dut):
     dut.inp29.value=in29
     dut.inp30.value=in30
 
-    dut.sel.value=sel
+    dut.sel.value=Sel
 
-    if (sel==0):
-        out=in0
-    elif (sel==1):
-        out=in1
-    elif (sel==2):
-        out=in2
-    elif (sel==3):
-        out=in3
-    elif (sel==4):
-        out=in4
-    elif (sel==5):
-        out=in5
-    elif (sel==6):
-        out=in6
-    elif (sel==7):
-        out=in7
-    elif (sel==8):
-        out=in8
-    elif (sel==9):
-        out=in9
-    elif (sel==10):
-        out=in11
-    elif (sel==12):
-        out=in12
-    elif (sel==13):
-        out=in13
-    elif (sel==14):
-        out=in14
-    elif (sel==15):
-        out=in15
-    elif (sel==16):
-        out=in16
-    elif (sel==17):
-        out=in17
-    elif (sel==18):
-        out=in18
-    elif (sel==19):
-        out=in19
-    elif (sel==20):
-        out=in20
-    elif (sel==21):
-        out=in21
-    elif (sel==22):
-        out=in22
-    elif (sel==23):
-        out=in23
-    elif (sel==24):
-        out=in24
-    elif (sel==25):
-        out=in25
-    elif (sel==26):
-        out=in26
-    elif (sel==27):
-        out=in27
-    elif (sel==28):
-        out=in28
-    elif (sel==29):
-        out=in29
-    elif (sel==30):
-        out=in30
-    assert dut.out.value==out,f"mux result is incorrect"
+
+    if (Sel==0):
+        Out=in0
+    elif (Sel==1):
+        Out=in1
+    elif (Sel==2):
+        Out=in2
+    elif (Sel==3):
+        Out=in3
+    elif (Sel==4):
+        Out=in4
+    elif (Sel==5):
+        Out=in5
+    elif (Sel==6):
+        Out=in6
+    elif (Sel==7):
+        Out=in7
+    elif (Sel==8):
+        Out=in8
+    elif (Sel==9):
+        Out=in9
+    elif (Sel==10):
+        Out=in11
+    elif (Sel==12):
+        Out=in12
+    elif (Sel==13):
+        Out=in13
+    elif (Sel==14):
+        Out=in14
+    elif (Sel==15):
+        Out=in15
+    elif (Sel==16):
+        Out=in16
+    elif (Sel==17):
+        Out=in17
+    elif (Sel==18):
+        Out=in18
+    elif (Sel==19):
+        Out=in19
+    elif (Sel==20):
+        Out=in20
+    elif (Sel==21):
+        Out=in21
+    elif (Sel==22):
+        Out=in22
+    elif (Sel==23):
+        Out=in23
+    elif (Sel==24):
+        Out=in24
+    elif (Sel==25):
+        Out=in25
+    elif (Sel==26):
+        Out=in26
+    elif (Sel==27):
+        Out=in27
+    elif (Sel==28):
+        Out=in28
+    elif (Sel==29):
+        Out=in29
+    elif (Sel==30):
+        Out=in30
+    
+    await Timer(2,units="ns")
+    dut._log.info(f'sel={Sel:05} out={Out:05} DUT={int(dut.out.value):05}')
+    assert dut.out.value==Out,f"mux result is incorrect"
    
-
-    //cocotb.log.info('##### CTB: Develop your test here ########')
