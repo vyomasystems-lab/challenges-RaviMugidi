a67f7d04e9fb65723785a7846be919c8dfe55aee
RaviMugidi <ravimugidi7@gmail.com>
2022-07-31 17:09:38 +0000

    initial commit

--- level3_design/Universal_shift_reg.v
+++ level3_design/Universal_shift_reg.v
@@ -4,21 +4,21 @@ module Universal_Reg(clk,serial_in,sel,inp,reset,out);
 	input serial_in;
 	input [3:0]inp;
 	input [1:0]sel;
-  output reg [3:0]out;
+    output reg [3:0]out;
 	
 	
 	always@(posedge clk)
 	begin
 		if(reset==1)
 			out<=0;
-	         else
-			 begin
-			 case(sel)
-	                    2'b00: out <= out;
-				 2'b01: out <= {out[2:0],serial_in};
-			    2'b10: out <= {serial_in,out[3:1]};
-			    2'b11: out <= inp;
-	                  endcase
-	                  end
+	    else
+			begin
+			    case(sel)
+	                2'b00: out <= out;
+				    2'b01: out <= {out[2:0],serial_in};
+			        2'b10: out <= {serial_in,out[3:1]};
+			        2'b11: out <= inp;
+	            endcase
+	         end
 	end
 endmodule
